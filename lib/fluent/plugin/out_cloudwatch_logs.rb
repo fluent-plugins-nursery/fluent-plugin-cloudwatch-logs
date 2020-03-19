0c69bd1e75a1fa9977fc46c93bdfd8f9884b61df
Hiroshi Hatake <hatake@clear-code.com>
2020-03-19 16:03:58 +0900

    Check log_groups existence with pagenation
    This should be needed because default response contains only 50
    log_group names.
    
    Signed-off-by: Hiroshi Hatake <hatake@clear-code.com>

--- lib/fluent/plugin/out_cloudwatch_logs.rb
+++ lib/fluent/plugin/out_cloudwatch_logs.rb
@@ -455,12 +455,23 @@ module Fluent::Plugin
     def log_group_exists?(group_name)
       if @sequence_tokens[group_name]
         true
-      elsif @logs.describe_log_groups.any? {|page| page.log_groups.any? {|i| i.log_group_name == group_name } }
+      elsif check_log_group_existence(group_name)
         @sequence_tokens[group_name] = {}
         true
       else
         false
       end
+    end
+
+    def check_log_group_existence(group_name)
+      response = @logs.describe_log_groups(log_group_name_prefix: group_name)
+      response.each {|page|
+        if page.log_groups.find {|i| i.log_group_name == group_name }
+          return true
+        end
+      }
+
+      false
     end
 
     def log_stream_exists?(group_name, stream_name)
